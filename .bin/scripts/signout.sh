3dc4d3af8866a6c124cdd21a7fe69944e3d50458
star-child-0 <andreavannini1@gmail.com>
2023-04-05 15:47:10 +0200

    See you space cowboy.

--- .bin/scripts/signout.sh
+++ .bin/scripts/signout.sh
@@ -3,17 +3,6 @@
 clear
 echo "Signing out..."
 
-# cd webchat/webchat
-# git add --all > /dev/null 2>&1
-# git commit -m "See you space cowboy." > /dev/null 2>&1
-# git push origin master > /dev/null 2>&1
-# cd ..
-
-# git add --all > /dev/null 2>&1
-# git commit -m "See you space cowboy." > /dev/null 2>&1
-# git push origin master > /dev/null 2>&1
-# echo "See you space cowboy."
-
 function add_commit_push(){
 	git add --all > /dev/null 2>&1
 	git commit -m "See you space cowboy." > /dev/null 2>&1
@@ -34,5 +23,6 @@ for file in */; do
 done
 
 add_commit_push
+echo "See you space cowboy."
 
 exit 0

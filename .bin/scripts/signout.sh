dc43d483d7213d0a702dc01fe2b476224ceb6b25
star-child-0 <andreavannini1@gmail.com>
2023-04-05 15:45:33 +0200

    See you space cowboy.

--- .bin/scripts/signout.sh
+++ .bin/scripts/signout.sh
@@ -1,15 +1,38 @@
+#!/bin/bash
+
 clear
 echo "Signing out..."
 
-cd webchat/webchat
-git add --all > /dev/null 2>&1
-git commit -m "See you space cowboy." > /dev/null 2>&1
-git push origin master > /dev/null 2>&1
-cd ..
+# cd webchat/webchat
+# git add --all > /dev/null 2>&1
+# git commit -m "See you space cowboy." > /dev/null 2>&1
+# git push origin master > /dev/null 2>&1
+# cd ..
 
-git add --all > /dev/null 2>&1
-git commit -m "See you space cowboy." > /dev/null 2>&1
-git push origin master > /dev/null 2>&1
-echo "See you space cowboy."
+# git add --all > /dev/null 2>&1
+# git commit -m "See you space cowboy." > /dev/null 2>&1
+# git push origin master > /dev/null 2>&1
+# echo "See you space cowboy."
+
+function add_commit_push(){
+	git add --all > /dev/null 2>&1
+	git commit -m "See you space cowboy." > /dev/null 2>&1
+	git push origin master > /dev/null 2>&1
+}
+
+for file in */; do
+	if [ "$file" == "webchat/" ]; then
+		if [ -d "$file/webchat" ]; then
+			cd $file/webchat
+			add_commit_push
+		else
+			cd $file
+			add_commit_push
+		fi
+		cd ..
+	fi
+done
+
+add_commit_push
 
 exit 0

5f68d61b80e2d1da92f579bdd2a569ab60375ba5
star-child-0 <andreavannini1@gmail.com>
2023-04-05 14:01:04 +0200

    See you space cowboy.

--- .bin/scripts/signout.sh
+++ .bin/scripts/signout.sh
@@ -1,7 +1,7 @@
 clear
 echo "Signing out..."
 
-cd webchat
+cd webchat/webchat
 git add --all > /dev/null 2>&1
 git commit -m "See you space cowboy." > /dev/null 2>&1
 git push origin master > /dev/null 2>&1
